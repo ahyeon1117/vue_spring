413b9d1436478ec3a1f89b9d24de8546483c1421
Hyeon <ahyeon1117@dtonic.io>
2023-08-22 12:48:00 +0900

    [feat] axios 추가

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,7 +4,12 @@ import "./assets/main.css";
 import { createApp } from "vue";
 import App from "./App.vue";
 import { router } from "./router/index.js";
+import axios from "axios";
 
 const app = createApp(App);
 
-app.use(router).mount("#app"); // 라우터 사용
+app
+  .use(router)
+  .provide("$axios", axios)
+  .provide("$router", router)
+  .mount("#app"); // 라우터 사용
--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -5,47 +5,70 @@
         <h1 class="loginBox_logoArea_logo mgT20">logo</h1>
       </div>
       <div class="loginBox_inputArea">
-        <div class="loginBox_inputArea_input-container">
-          <label :class="idClass">전화번호, 사용자 이름 또는 이메일</label>
-          <input v-model="data.input.id" class="mgT4" />
-        </div>
-        <div class="loginBox_inputArea_input-container">
-          <label :class="pwClass">비밀번호</label>
-          <input v-model="data.input.pw" class="mgT4" type="password" />
-        </div>
-        <button class="mgT12" type="">로그인</button>
+        <form @submit="loginUser">
+          <div class="loginBox_inputArea_input-container">
+            <label :class="data.formClass.id"
+              >전화번호, 사용자 이름 또는 이메일</label
+            >
+            <input v-model="data.input.id" class="mgT4" />
+          </div>
+          <div class="loginBox_inputArea_input-container">
+            <label :class="data.formClass.pw">비밀번호</label>
+            <input v-model="data.input.pw" class="mgT4" type="password" />
+          </div>
+          <button class="mgT12" type="submit" :disabled="data.disable.login">
+            로그인
+          </button>
+        </form>
       </div>
-      <hr />
     </div>
   </div>
 </template>
 <script setup>
-import { computed, reactive } from "vue";
+import { watch, reactive } from "vue";
+import { inject } from "vue";
+const $router = inject("$router");
 
 const data = reactive({
   input: {
     id: "",
     pw: ""
+  },
+  disable: {
+    login: true
+  },
+  formClass: {
+    id: "loginBox_inputArea_label",
+    pw: "loginBox_inputArea_label"
   }
 });
-const idClass = computed(() => {
-  console.log(
-    data.input.id.length > 0
-      ? "loginBox_inputArea_smallLabel"
-      : "loginBox_inputArea_label"
-  );
-  return data.input.id.length > 0
-    ? "loginBox_inputArea_smallLabel"
-    : "loginBox_inputArea_label";
-});
-const pwClass = computed(() => {
-  console.log(
-    data.input.pw.length > 0
-      ? "loginBox_inputArea_smallLabel"
-      : "loginBox_inputArea_label"
-  );
-  return data.input.pw.length > 0
-    ? "loginBox_inputArea_smallLabel"
-    : "loginBox_inputArea_label";
-});
+watch(
+  () => data.input,
+  (newValues, prevValues) => {
+    if (newValues.id.length > 0) {
+      data.formClass.id = "loginBox_inputArea_smallLabel";
+    } else {
+      data.formClass.id = "loginBox_inputArea_label";
+    }
+    if (newValues.pw.length > 0) {
+      data.formClass.pw = "loginBox_inputArea_smallLabel";
+    } else {
+      data.formClass.pw = "loginBox_inputArea_label";
+    }
+    if (validPw(newValues.pw) && validId(newValues.id)) {
+      data.disable.login = false;
+    }
+  },
+  { deep: true }
+);
+const loginUser = () => {
+  $router.push("/main");
+};
+
+const validId = (id) => {
+  return id.length > 0;
+};
+const validPw = (pw) => {
+  return pw.length > 0;
+};
 </script>
